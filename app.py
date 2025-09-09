972f4bd1efc728f9f138e2d959d18c073eaa2e3d
Sabrina Rybak <151574678+sabrinarybak@users.noreply.github.com>
2025-09-09 12:21:09 +0200

    fix!

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from import Flask
+from flask import Flask
 import os
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -17,5 +16,5 @@ def sabrina():
     return "Hello Rainbow 6! This is Sabrina"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
